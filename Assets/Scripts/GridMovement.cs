b24756d668f70a012ce6ac7d53e0b52e0265b380
Måns Fritiofsson <mans@fritiofson.net>
2024-10-18 22:22:47 +0200

    Work in progress player rotation :)

--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,29 +10,79 @@ public class GridMovement : MonoBehaviour
     [SerializeField] Tilemap tilemap;
     [SerializeField] AnimationCurve movementInterpolationCurve;
     [SerializeField] float movementInterpolationDuration;
+    [SerializeField] float rotationInterpolationDuration;
+
     [SerializeField] Vector3 positionOffset;
 
     Vector3Int position;
-    float interpolationStartTime;
-    Vector3 interpolationStartPosition;
-    Vector3 interpolationTargetPosition;
-    bool interpolating = false;
+    float positionInterpolationStart;
+    float rotationInterpolationStart;
+
+    Vector3 posInterpolationStartValue;
+    Vector3 posInterpolationTargetValue;
+    Quaternion rotInterpolationStartValue;
+    Quaternion rotInterpolationTargetValue;
+    bool moving = false;
+    bool rotating = false;
+    Direction facingDirection;
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    Vector3Int DirectionToMovement(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.North => Vector3Int.forward,
+            Direction.East => Vector3Int.right,
+            Direction.West => Vector3Int.left,
+            Direction.South => Vector3Int.back,
+            _ => Vector3Int.zero
+        };
+    }
+    Quaternion DirectionToQuaternion(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.North => Quaternion.Euler(0, 0, 0),
+            Direction.East => Quaternion.Euler(0, 0, 270),
+            Direction.West => Quaternion.Euler(0, 0, 90),
+            Direction.South => Quaternion.Euler(0, 0, 180),
+            _ => Quaternion.identity
+        };
+    }
+    /*Direction RotateDirection(Direction direction, bool reverse)
+    {
+        return direction switch
+        {
+            Direction.North => Quaternion.Euler(0, 0, 0),
+            Direction.East => Quaternion.Euler(0, 0, 270),
+            Direction.West => Quaternion.Euler(0, 0, 90),
+            Direction.South => Quaternion.Euler(0, 0, 180),
+            _ => Quaternion.identity
+        };
+    }*/
+
 
     void Update()
     {
-        Vector3Int movement = new
-        (
-            Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")),
-            Mathf.RoundToInt(Input.GetAxisRaw("Vertical")),
-            0
-        );
-        Move(movement);
+        int movementInput = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+        if (movementInput == 1)
+            Move(DirectionToMovement(facingDirection));
+        else if (movementInput == -1)
+            Move(-DirectionToMovement(facingDirection));
+
         InterpolatePosition();
+        InterpolateRotation();
     }
 
     public void Move(Vector3Int translation)
     {
-        if (translation == Vector3Int.zero || interpolating) return;
+        if (translation == Vector3Int.zero || moving) return;
 
         Vector3Int nextPosition = position + translation;
         DungeonTile nextTile = tilemap.GetTile<DungeonTile>(nextPosition);
@@ -38,18 +90,42 @@ public class GridMovement : MonoBehaviour
         if (nextTile == null || !nextTile.Walkable) return;
 
         position = nextPosition;
-        interpolationStartPosition = transform.position;
-        interpolationTargetPosition = tilemap.CellToWorld(position) + positionOffset;
-        interpolationStartTime = Time.time;
-        interpolating = true;
+        posInterpolationStartValue = transform.position;
+        posInterpolationTargetValue = tilemap.CellToWorld(position) + positionOffset;
+        positionInterpolationStart = Time.time;
+        moving = true;
     }
+    /*public void Rotate(Direction toDirection)
+    {
+        if (translation == Vector3Int.zero || moving) return;
+
+        Vector3Int nextPosition = position + translation;
+        DungeonTile nextTile = tilemap.GetTile<DungeonTile>(nextPosition);
+
+        if (nextTile == null || !nextTile.Walkable) return;
+
+        position = nextPosition;
+        posInterpolationStartValue = transform.position;
+        posInterpolationTargetValue = tilemap.CellToWorld(position) + positionOffset;
+        positionInterpolationStart = Time.time;
+        moving = true;
+    }*/
 
     void InterpolatePosition()
     {
-        if (!interpolating) return;
-        float progress = (Time.time - interpolationStartTime) / movementInterpolationDuration;
+        if (!moving) return;
+        float progress = (Time.time - positionInterpolationStart) / movementInterpolationDuration;
 
-        transform.position = Vector3.Lerp(interpolationStartPosition, interpolationTargetPosition, movementInterpolationCurve.Evaluate(progress));
-        if (progress >= 1f) interpolating = false;
+        transform.position = Vector3.Lerp(posInterpolationStartValue, posInterpolationTargetValue, movementInterpolationCurve.Evaluate(progress));
+        if (progress >= 1f) moving = false;
+    }
+    void InterpolateRotation()
+    {
+        if (!rotating) return;
+        float progress = (Time.time - rotationInterpolationStart) / rotationInterpolationDuration;
+
+        float rotationZ = Mathf.Lerp(rotInterpolationStartValue, rotInterpolationTargetValue, movementInterpolationCurve.Evaluate(progress));
+        transform.eulerAngles = new Vector3(0, 0, rotationZ);
+        if (progress >= 1f) rotating = false;
     }
 }
