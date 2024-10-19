37d939019022171a7cea5f40dd30155133c217b2
Måns Fritiofsson <mans@fritiofson.net>
2024-10-19 17:26:27 +0200

    Improved rat throwing

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,6 +7,7 @@ public class PlayerAttack : MonoBehaviour
 {
 
     [SerializeField] RatProjectile ratProjectile;
+    [SerializeField] Animator HandAnimator;
     bool ratInHand = true;
 
     PlayerMovement playerMovement;
@@ -32,13 +33,22 @@ public class PlayerAttack : MonoBehaviour
             ratProjectile.transform.position = playerMovement.WorldPosition;
             ratProjectile.transform.rotation = GridMovement.DirectionToQuaternion(playerMovement.facingDirection);
             ratProjectile.Throw();
+            HandAnimator.Play("Thrown");
             ratInHand = false;
+            Invoke(nameof(HideHand), 0.25f);
         }
     }
 
     void OnRatReturn()
     {
         ratInHand = true;
-        ratProjectile.transform.parent = transform;
+        CancelInvoke(nameof(HideHand));
+        HandAnimator.Play("Held");
+        ratProjectile.transform.parent = null;
+    }
+
+    void HideHand()
+    {
+        if (!ratInHand) HandAnimator.Play("Hidden");
     }
 }
