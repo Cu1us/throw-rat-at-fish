5b37fc387615930781e6886f890960bd20e72a06
Måns Fritiofsson <mans@fritiofson.net>
2024-10-19 19:58:31 +0200

    Keycard hand & enemy spawnpoint tile

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : GridMovement
 {
     public byte Keys = 0;
+    [SerializeField] Image KeycardHandUI;
     protected override void Update()
     {
         int movementInput = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
@@ -17,7 +19,7 @@ public class PlayerMovement : GridMovement
 
         if (rotationInput != 0)
             Rotate(clockwise: rotationInput == -1);
-
+        KeycardHandUI.enabled = Keys > 0;
         base.Update();
     }
     protected override bool TryWalkOnTile(Vector3Int position)
